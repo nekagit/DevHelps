da8ead4d87d582b7227b47649e1975a3ed0bf585
nekagit <nenad99@hotmail.de>
2023-09-09 13:28:37 +0200

    asfd

---
+++ src/api/server.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-undef */
+import { exec } from "child_process";
+import cors from "cors";
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Function to execute a Git script
+const executeGitScript = (scriptName, branchName) => {
+  return new Promise((resolve, reject) => {
+    // Define the path to the script file (assuming it's in the src/scripts directory)
+    const scriptPath = path.join(
+      "C:\\Users\\Nenad\\Desktop\\DevsHelp\\DevHelps\\src\\scripts",
+      scriptName
+    );
+
+    // Execute the script with the branch name as an argument
+    exec(`"${scriptPath}" "${branchName}"`, (error) => {
+      if (error) {
+        reject({
+          success: false,
+          message: `Error executing Git script: ${error.message}`,
+        });
+      } else {
+        resolve({
+          success: true,
+          message: "Git script executed successfully",
+        });
+      }
+    });
+  });
+};
+
+
+// Endpoint to execute a Git script by name
+app.post("/execute-git-script", async (req, res) => {
+  try {
+    const { scriptName, branchName } = req.body;
+
+    const result = await executeGitScript(scriptName, branchName);
+    if (result.success) {
+      res.status(200).json({ message: "Git script executed successfully" });
+    } else {
+      res.status(500).json({ error: result.message });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- src/components/GitHubCard.tsx
+++ src/components/GitHubCard.tsx
@@ -1,4 +1,5 @@
-import { Card, ListGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card, Form, ListGroup } from "react-bootstrap";
 
 interface IProps {
   title: string;
@@ -11,6 +12,34 @@ interface IProps {
 function BasicCard(props: IProps) {
   const { title, width, height, color, border } = props;
   const borderStyle = "1px solid black";
+  const [branchName, setBranchName] = useState("");
+  console.log(branchName);
+  const handleGitAction = async (scriptName: string) => {
+    try {
+      const response = await fetch("http://localhost:3000/execute-git-script", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ scriptName, branchName }), // Include branchName in the request body
+      });
+
+      if (response.ok) {
+        console.log(`Git script "${scriptName}" executed successfully.`);
+      } else {
+        console.error("Error executing Git script.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleBranchNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setBranchName(event.target.value);
+  };
+
   return (
     <>
       <Card
@@ -24,16 +53,27 @@ function BasicCard(props: IProps) {
         <Card.Header>{title}</Card.Header>
         <Card.Body>
           <ListGroup>
-            <ListGroup.Item>git create Branch from develop</ListGroup.Item>
-
-            <ListGroup.Item>git sync all with develop</ListGroup.Item>
-            <ListGroup.Item>git commit as "new Implementation"</ListGroup.Item>
-            <ListGroup.Item>git commit as "Update" + Reason</ListGroup.Item>
-            <ListGroup.Item>
-              git commit as Comments or Tests failed
-            </ListGroup.Item>
-            <ListGroup.Item>git push</ListGroup.Item>
-            <ListGroup.Item>git pull</ListGroup.Item>
+            {/* Input field for branch name */}
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="Branch Name"
+                value={branchName}
+                onChange={handleBranchNameChange}
+              />
+            </Form.Group>
+            <Button onClick={() => handleGitAction("create-branch.bat")}>
+              Create Branch from 'develop'
+            </Button>
+            <Button onClick={() => handleGitAction("sync-develop.bat")}>
+              Sync All with 'develop'
+            </Button>
+            <Button
+              onClick={() => handleGitAction("commit-new-implementation.bat")}
+            >
+              Commit as "new Implementation"
+            </Button>
+            {/* Add more ListGroup items with corresponding script names */}
           </ListGroup>
         </Card.Body>
       </Card>

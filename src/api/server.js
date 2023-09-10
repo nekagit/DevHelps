5cd696a82244befe63e7812225036ed88d25e0d6
nekagit <nenad99@hotmail.de>
2023-09-10 22:35:07 +0200

    asdf

--- src/api/server.js
+++ src/api/server.js
@@ -1,64 +1,50 @@
-/* eslint-disable no-async-promise-executor */
 /* eslint-disable no-undef */
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import cors from "cors";
 import express from "express";
 import path from "path";
 
 const app = express();
 const port = 3000;
-let isExecutingScript = false; // Flag to track if a script is currently executing
 
 app.use(express.json());
 app.use(cors());
 
 // Function to execute a Git script sequentially
-const executeGitScriptsSequentially = (scriptName, scriptParameters) => {
-  if (isExecutingScript) {
-    return Promise.reject({
-      success: false,
-      message: "Another script is already executing.",
-    });
-  }
-
-  return new Promise( async (resolve, reject) => {
-    isExecutingScript = true;
-    try {
-      const scriptPath = path.join(
-        "C:\\Users\\Nenad\\Desktop\\DevsHelp\\DevHelps\\src\\scripts",
-        scriptName
-      );
-
-      for (let scriptParameter of scriptParameters) {
-        console.log(`Executing script with parameter: ${scriptParameter}`);
-        await executeScript(scriptPath, scriptParameter);
-      }
-
-      resolve({
-        success: true,
-        message: "All Git scripts executed successfully",
-      });
-    } catch (error) {
-      reject(error);
-    } finally {
-      isExecutingScript = false;
-    }
-  });
-};
-
-// Function to execute a single script
-const executeScript = (scriptPath, scriptParameter) => {
+const executeGitScript = (scriptName, scriptParameters) => {
   return new Promise((resolve, reject) => {
-    exec(`"${scriptPath}" "${scriptParameter}"`, (error) => {
-      if (error) {
-        reject({
-          success: false,
-          message: `Error executing Git script: ${error.message}`,
-        });
-      } else {
+    // Define the path to the script file (assuming it's in the src/scripts directory)
+    const scriptPath = path.join(
+      "C:\\Users\\Nenad\\Desktop\\DevsHelp\\DevHelps\\src\\scripts",
+      scriptName
+    );
+      console.log(scriptParameters)
+    // Create a terminal instance to run the script
+    const terminal = spawn("cmd.exe", ["/c", scriptPath, ...scriptParameters]);
+
+    // Capture the output of the terminal
+    let output = "";
+
+    terminal.stdout.on("data", (data) => {
+      output += data.toString();
+    });
+
+    terminal.stderr.on("data", (data) => {
+      output += data.toString();
+    });
+
+    terminal.on("close", (code) => {
+      if (code === 0) {
         resolve({
           success: true,
           message: "Git script executed successfully",
+          output,
+        });
+      } else {
+        reject({
+          success: false,
+          message: `Error executing Git script. Exit code: ${code}`,
+          output,
         });
       }
     });
@@ -69,20 +55,12 @@ const executeScript = (scriptPath, scriptParameter) => {
 app.post("/execute-git-script", async (req, res) => {
   try {
     const { scriptName, scriptParameters } = req.body;
-
-    const result = await executeGitScriptsSequentially(
-      scriptName,
-      scriptParameters
-    );
-
-    if (result.success) {
-      res.status(200).json({ message: result.message });
-    } else {
-      res.status(500).json({ error: result.message });
-    }
+    console.log(scriptParameters)
+    const result = await executeGitScript(scriptName, scriptParameters);
+    res.status(200).json(result);
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "Server error" });
+    res.status(500).json(error);
   }
 });
 

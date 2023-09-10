d5f3c858e584ba9aa8c8c8a1b5578eaa1be4a057
nekagit <nenad99@hotmail.de>
2023-09-10 12:50:22 +0200

    Auto-commit changes

---
+++ src/scripts/create-branch.bat
@@ -0,0 +1,37 @@
+@echo off
+
+REM Change to the directory where your Git repository is located
+cd C:\Users\Nenad\Desktop\DevsHelp\DevHelps
+
+REM Stage all changes, including new files
+git add --all
+
+REM Commit the changes with an auto-generated message
+git commit -m "Auto-commit changes"
+
+REM Attempt to checkout 'develop'
+git checkout develop 2>nul
+
+REM If the checkout to 'develop' fails, try 'master'
+IF %ERRORLEVEL% NEQ 0 (
+  git checkout master 2>nul
+)
+
+REM If the checkout to 'master' fails, try 'main'
+IF %ERRORLEVEL% NEQ 0 (
+  git checkout main 2>nul
+)
+
+REM Sync with the checked-out branch (pull changes)
+git pull
+
+REM Get the new branch name from the command line argument (e.g., 'new-branch-name')
+SET newBranchName=%1
+
+REM Create a new branch with the specified name
+IF NOT "%newBranchName%"=="" (
+  git checkout -b %newBranchName%
+)
+
+REM Exit the batch script
+exit /b 0

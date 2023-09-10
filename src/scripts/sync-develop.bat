9a619f6c08520f48c09f5efd23780bf98b1d4ca4
nekagit <nenad99@hotmail.de>
2023-09-10 22:11:41 +0200

    asdf

--- src/scripts/sync-develop.bat
+++ src/scripts/sync-develop.bat
@@ -5,19 +5,12 @@ cd C:\Users\Nenad\Desktop\DevsHelp\DevHelps
 
 REM Check the current branch name
 for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set current_branch=%%i
-echo %current_branch%
 REM Check if the current branch is one of 'develop', 'master', or 'main'
 If NOT "%current_branch%"=="master" (
   git add --all
-  REM Check if there are any changes to commit
-  git diff-index --quiet HEAD
-  IF %ERRORLEVEL% NEQ 0 (
-      REM Commit the changes with a default message
-      git commit -m "Auto-commit changes"
-      git pull
-      git push
-  ) 
-
+  git commit -m "Auto-commit changes"
+  git pull
+  git push
 )
 
 REM Attempt to checkout 'develop'
@@ -36,15 +29,6 @@ echo %branchName%
 
 REM Merge with 'develop'
 git merge master
-
-REM Check if the remote branch exists before pushing
-git rev-parse --verify --quiet origin/%branchName%
-IF %ERRORLEVEL% EQU 0 (
-  REM Push changes to the remote 'origin'
-  git push origin %branchName%
-) ELSE (
-  echo Remote branch '%branchName%' does not exist. Skipping push.
-)
 
 REM Exit the batch script
 exit /b 0
